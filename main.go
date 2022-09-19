9b538643da1bd70ba4aa00b53ddd0d3d8cf3b95d
tursynkhan <tursynkhan001@gmail.com>
2022-09-19 19:12:33 +0600

    linear-stats

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"strconv"
 )
@@ -31,6 +32,7 @@ func main() {
 		arr = append(arr, float64(elem))
 	}
 	LinRegLine(arr)
+	PearsonCoef(arr)
 }
 
 func LinRegLine(arr []float64) {
@@ -38,29 +40,35 @@ func LinRegLine(arr []float64) {
 	sum_y := 0.0
 	sum_xy := 0.0
 	sum_xx := 0.0
-	count := 0.0
+	p := float64(len(arr))
 	for i := 0; i < len(arr); i++ {
 		sum_x += float64(i)
 		sum_y += arr[i]
 		sum_xx += float64(i * i)
-		sum_xy += arr[i] * arr[i]
-		count++
-	}
-	m := (count*sum_xy - sum_x*sum_y) / (count*sum_xx - sum_x*sum_x)
-	b := (sum_y / count) - (m*sum_x)/count
+		sum_xy += float64(i) * arr[i]
 
-	result_x := []float64{}
-	result_y := []float64{}
-
-	for j := 0; j < len(arr); j++ {
-		x := arr[j]
-		y := x*m + b
-		result_x = append(result_x, x)
-		result_y = append(result_y, y)
 	}
-	fmt.Println(result_x)
-	fmt.Println(result_y)
+	m := (p*sum_xy - sum_x*sum_y) / (p*sum_xx - sum_x*sum_x)
+	b := (sum_y / p) - (m*sum_x)/p
+
+	fmt.Printf("Linear Regression Line: y = %0.6fx + %0.6f\n", m, b)
 }
 
-// func PearsonCoef(arr []float64) {
-// }
+func PearsonCoef(arr []float64) {
+	sum_x := 0.0
+	sum_y := 0.0
+	sum_xy := 0.0
+	squareSum_x := 0.0
+	squareSum_y := 0.0
+	n := float64(len(arr))
+	for i := 0; i < len(arr); i++ {
+		sum_x += float64(i)
+		sum_y += arr[i]
+		sum_xy += float64(i) * arr[i]
+
+		squareSum_x += float64(i) * float64(i)
+		squareSum_y += arr[i] * arr[i]
+	}
+	corr := float64((n*sum_xy - sum_x*sum_y)) / (math.Sqrt(float64((n*squareSum_x - sum_x*sum_x) * (n*squareSum_y - sum_y*sum_y))))
+	fmt.Printf("Pearson Correlation Coefficient: %0.10f\n", corr)
+}
